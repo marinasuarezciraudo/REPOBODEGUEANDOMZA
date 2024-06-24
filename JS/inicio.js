299a7c026e8ce329e904cdd9ef2a7c84b27fcc8b
Marina Suarez <marinaciraudo@gmail.com>
2024-06-24 18:22:17 -0300

    segunda entrega

---
+++ JS/inicio.js
@@ -0,0 +1,9 @@
+var respuesta = window.prompt("Descubre el corazón vitivinícola de Argentina!!!", " Si, por supuesto! Salud! 🍷​🍷​ ");
+alert ("Su respuesta fue " + respuesta);
+
+var sitioweb = "BodegueandoMendoza";
+var bodegas = ("BodegaBianchi" + "BodegaViamonte");
+
+
+console.log("SitioWeb: "+ sitioweb + "" + typeof sitioweb);
+console.log("Bodegas:" + bodegas + "" + typeof bodegas);
