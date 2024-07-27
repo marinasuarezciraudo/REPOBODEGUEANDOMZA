51f44c882d144c80dd357c6ef295bf63a9bf15a9
Marina Suarez <marinaciraudo@gmail.com>
2024-07-27 16:17:34 -0300

    cuarta entrega

---
+++ JS/bianchi.js
@@ -0,0 +1,15 @@
+function capturarDatos(){
+    let inputEmail = document.getElementById('email');
+    let formulario = document.querySelector('form');
+    }
+
+    formulario.addEventListener('enviar'), function(evento) {
+        evento.preventDefault();
+        // Código para validar el formulario
+        // Código para enviar los datos del formulario
+        console.log(inputEmail.value)
+        }
+        let usuario= {
+            email: inputEmail.value
+            }
+     
--- JS/inicio.js
+++ JS/inicio.js
@@ -1,9 +1,24 @@
-var respuesta = window.prompt("Descubre el corazón vitivinícola de Argentina!!!", " Si, por supuesto! Salud! 🍷​🍷​ ");
-alert ("Su respuesta fue " + respuesta);
+//var respuesta = window.prompt("Descubre el corazón vitivinícola de Argentina!!!", " Si, por supuesto! Salud! 🍷​🍷​ ");
+//alert ("Su respuesta fue " + respuesta);
 
-var sitioweb = "BodegueandoMendoza";
-var bodegas = ("BodegaBianchi" + "BodegaViamonte");
+//var sitioweb = "BodegueandoMendoza";
+//var bodegas = ("BodegaBianchi" + "BodegaViamonte");
 
 
-console.log("SitioWeb: "+ sitioweb + "" + typeof sitioweb);
-console.log("Bodegas:" + bodegas + "" + typeof bodegas);
+//console.log("SitioWeb: "+ sitioweb + "" + typeof sitioweb);
+//console.log("Bodegas:" + bodegas + "" + typeof bodegas);
+
+//captura de datos de formmulario de suscripcion//
+
+  let formulario = document.getElementById("form");
+  console.log(formulario);
+ 
+ formulario.addEventListener('submit', function (captura){
+  captura.preventDefault();
+     let email= document.querySelector('#emailsuscrip').value;
+        console.log (email);
+     
+  })
+  
+   
+  
